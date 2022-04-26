eabad64cfb2d08777a7af666ca0caee13436ae9f
Sanjay-Khadka <sanjay1khadka@gmail.com>
2022-04-26 23:21:56 +0545

    validation

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
-import authRouter from "./routes/auth.js";
 import User from "./models/users.js";
 import realAuth from "./routes/realAuth.js";
-import Joi from "joi";
 
 const app = express();
 
 app.use(express.json());
-
-// 'mongosh "mongodb+srv://covidapp.4aptm.mongodb.net/CovidApp" --apiVersion 1 --username sanjaykhadka'
 mongoose
   .connect("mongodb://localhost:27017/user")
   .then(() => console.log("connected to db"))
   .catch((error) => console.log("failed"));
 
-// app.use("/user", authRouter);
 app.use("/users", realAuth);
 
 app.get("/", (req, res) => {
--- routes/realAuth.js
+++ routes/realAuth.js
@@ -1,7 +1,14 @@
 import express from "express";
 import User from "../models/users.js";
+import Joi from "joi";
 const router = express.Router();
 router.use(express.json());
+
+const schema = {
+  fullname: Joi.string().min(6).required,
+  email: Joi.string().min(7).required().email(),
+  password: Joi.string().min(6).required,
+};
 
 router.post("/register", async (req, res) => {
   const users = new User({
