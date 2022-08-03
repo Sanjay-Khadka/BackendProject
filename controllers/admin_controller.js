19739e37078aa3dedfc2344a7221d56c7f0903e4
Sanjay-Khadka <sanjay1khadka@gmail.com>
2022-08-03 12:44:11 +0545

    make admin controller created and routes handled

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -5,41 +5,29 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import Joi from "joi";
 
-export const createAdmin = async (req, res) => {
-  const { error } = registerValidation(req.body);
-  if (error) {
-    return res.status(400).send(error.message);
-  }
-  const emailExists = await User.findOne({ email: req.body.email });
-  if (emailExists) {
-    return res.status(400).send("email already exists");
-  }
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
-  const users = new User({
-    fullname: req.body.fullname,
-    email: req.body.email,
-    password: hashedPassword,
-  });
-  try {
-    const savedUser = await users.save();
-    res.status(200).send({ uid: savedUser._id });
-    console.log("user was created");
-  } catch {
-    (error) => console.log(error);
-    res.status(400).send("User Registration failed");
-  }
-};
-
 export const getUsers = async (req, res) => {
   try {
-    const allUsers = await User.find({});
+    const allUsers = await User.find({ isAdmin: false });
     res.send(allUsers);
     console.log("all users fetched");
   } catch (err) {
     res.status(404).json({ error: "could not get users", err });
     console.log("all user data fetching failed");
+  }
+};
+
+export const makeAdmin = async (req, res) => {
+  try {
+    const admin = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json({ success: "making admin successfull" });
+  } catch {
+    res.status(400).json({ error: "couldnot make admin" });
   }
 };
 
--- routes/admin_routes.js
+++ routes/admin_routes.js
@@ -1,20 +1,21 @@
 import express from "express";
 import {
-  createAdmin,
   updateUser,
   deleteUser,
   getUsers,
+  makeAdmin,
 } from "../controllers/admin_controller.js";
 import verify from "../models/verifyToken.js";
 
 const router = express.Router();
 router.use(express.json());
 
-router.post("/register", createAdmin);
+// router.post("/register", createAdmin);
 
 router.get("/users", getUsers);
 
 router.delete("/user/:id", verify, deleteUser);
 
 router.put("/user/:id", verify, updateUser);
+router.post("/makeadmin/:id", makeAdmin);
 export default router;
